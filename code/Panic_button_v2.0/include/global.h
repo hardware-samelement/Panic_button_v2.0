b137817817508ea13db6a2280a74a20d7441cec3
hardware <hardware@samelement.com>
2025-03-24 04:48:58 +0000

    change directory

---
+++ code/Panic_button_v2.0/include/global.h
@@ -0,0 +1,74 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include <Arduino.h>
+
+#define PIN_WIFI_RESET     26
+#define PIN_BUTTON_MAIN    27
+#define PIN_BUZZER         25
+#define PIN_BAT_ADC        36
+#define PIN_BAT_ADC_ENABLE 32
+#define PIN_BAT_CHARGING   34
+#define PIN_LED            33
+#define PIN_RS485_ENABLE   13
+#define PIN_WAKE_UP        GPIO_NUM_27
+
+#define HOUR_MULTIPLIER 3600000000ULL
+
+// sirene Serial Number, empty string if not used
+// #define SIRENE_SN "1S1537370"
+// #define SIRENE_SN "1S1547226" // sirine manyar
+#define SIRENE_SN ""
+
+enum ButtonType {
+  BUTTON_NC_LOCKED,
+  BUTTON_NO_MOMENTARY
+}
+
+enum ConnectivityType {
+  CONNECTION_WiFi,
+  CONNECTION_ESPNOW
+
+}
+
+typedef struct {
+  bool emergency;
+  bool button;
+  bool mqtt;
+  bool apMode;
+} DeviceStatus;
+
+typedef struct {
+  bool isCharging;
+  float voltage;
+  int percentage;
+} Battery;
+
+typedef struct {
+  int onDelay;
+  int offDelay;
+} Led;
+
+enum MessageType {
+  DATA,
+  PAIRING
+};
+
+enum PairStatus {
+  UNPAIRED,
+  PAIRED
+};
+
+struct Button {
+  int pin;
+  Button(int v) : pin(v) {}
+
+  bool read() {
+    return digitalRead(pin);
+  }
+
+  bool pressed;
+  bool longPressed;
+  unsigned long startTime;
+};
+#endif
---
+++ code/Panic_button_v2.0/include/my_espnow.h
@@ -0,0 +1,42 @@
+#ifndef MY_ESPNOW_H
+#define MY_ESPNOW_H
+
+#include "global.h"
+#include <WiFi.h>
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <stdint.h>
+
+enum DeviceModel {
+  SAM_EB = 0, // emergency Button
+  SAM_EB_Mini
+};
+
+typedef struct {
+  uint8_t msgType;
+  uint8_t model = SAM_EB;
+  bool emergencyStatus;
+  bool batteryStatus;
+  uint8_t batteryPercentage;
+} Struct_Data;
+
+typedef struct {
+  uint8_t msgType;
+  uint8_t macAddr[6];
+  uint8_t model;
+  uint8_t channel;
+  uint8_t status;
+  char serial[32];
+  char name[32];
+} Struct_Pair;
+
+void my_espnow_init(void);
+void my_espnow_pairing(void);
+
+void getMacAddress(void);
+String getSerial();
+
+void onDataSent(const uint8_t *mac, esp_now_send_status_t status);
+void onDataReceive(const uint8_t *mac, const uint8_t *incomingData, int len);
+
+#endif
